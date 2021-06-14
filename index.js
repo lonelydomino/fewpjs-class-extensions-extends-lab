5c7f2a0551078ba0e7ac1ae26b22f1f3f9f6e3b7
Michael Martinez <zeroeclipse@live.com>
2021-06-13 23:54:44 -0700

    commit tree c61350b49e07217f0565cb259e2584e5c6c75440

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+
+class Polygon {
+    constructor(sides){
+        this.sides = sides;
+        this.count = this.sides.length;
+    }
+    get countSides(){
+        return this.sides.length
+    }
+    get perimeter(){
+        let sum = 0
+        this.sides.forEach(element => {
+            sum+= element
+        });
+        return sum
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if(this.count != 3){
+            return false
+        }
+        else {
+            return ((this.sides[0] + this.sides[1] > this.sides[2]) && (this.sides[0] + this.sides[2] > this.sides[1]) && (this.sides[1] + this.sides[2] > this.sides[0]))
+        }
+    }
+}
+
+class Square extends Polygon {
+    get area() {
+     
+        if (this.count !== 4){
+            return false;  
+        } 
+        return this.sides[0] * this.sides[0]
+      }
+      get isValid() {
+        if (this.count !== 4){
+            return false;  
+        } 
+        return ((this.sides[0] === this.sides[1]) && (this.sides[0] === this.sides[2]) && (this.sides[0] === this.sides[3]))
+      }
+
+}
